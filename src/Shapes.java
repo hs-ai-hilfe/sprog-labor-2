691ed19f59b9aa5e1806b97a125c6ef512b0eefa
hs-ai-hilfe <github-hs-ai-hilfe@fenderdev.com>
2017-10-11 17:34:38 +0200

    wip

--- src/Shapes.java
+++ src/Shapes.java
@@ -1,17 +1,40 @@
 
 public class Shapes {
     
-    static int rectArea(int a, int b) {
+    /**
+     * Calculates the area of a rect
+     * 
+     * @param a Side a
+     * @param b Side b
+     * @return the area
+     */
+    static int areaRect(int a, int b) {
         int result = a * b;
         return result;
     }
     
-    static int volumen(int a, int b, int c) {
-        int result = rectArea(a, b) * c;
+    /**
+     * Calculates the volume of a cuboid
+     * 
+     * @param a Side a
+     * @param b Side b
+     * @param c Side c
+     * @return the volume of a cuboid
+     */
+    static int volumeCuboid(int a, int b, int c) {
+        int result = areaRect(a, b) * c;
         return result;
     }
     
-    static int oberflaeche(int a, int b, int c) {
+    /**
+     * Calculates the surface of a cuboid
+     * 
+     * @param a Side a
+     * @param b Side b
+     * @param c Side c
+     * @return the surface of a cuboid
+     */
+    static int surfaceCuboid(int a, int b, int c) {
         return 2 * (a * b + b * c + a * c);
     }
 
